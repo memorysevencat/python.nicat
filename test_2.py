6810481a9ecfa407349b82c817fee6dd1c2bd612
seven.cat <490542807@qq.com>
2016-03-13 11:15:52 +0800

    Delete test_2.py

--- test_2.py
+++
@@ -1,7 +0,0 @@
-globals={'x':7,'y':10,'core':[10,11,12]}
-locals={'x':1,'y':10,'core':[10,15,12]}
-a=eval('3*x+4*y',globals,locals)
-print a
-str='print 1+2'
-exec 'for b in core:print b' in globals,locals
-exec compile(str,'','exec') 
